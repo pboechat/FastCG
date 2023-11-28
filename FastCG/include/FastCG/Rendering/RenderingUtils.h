05c8afd3f889283d41b67f3cbb7a54e0cbb4ce33
Pedro Boechat <pboechat@gmail.com>
2023-11-28 18:08:57 +0000

    Fixed a silly error introduced by the fact that the SKYBOX render group was moved to the beginning of the rendergroup enumerator.

--- FastCG/include/FastCG/Rendering/RenderingUtils.h
+++ FastCG/include/FastCG/Rendering/RenderingUtils.h
@@ -15,12 +15,12 @@ namespace FastCG
         SKYBOX = 1,
         RESERVED = 2,
         OPAQUE_MATERIAL = 3,
-        TRANSPARENT_MATERIAL = (std::numeric_limits<RenderGroupInt>::max() / 2) + OPAQUE_MATERIAL,
+        TRANSPARENT_MATERIAL = (std::numeric_limits<RenderGroupInt>::max() / 2) + OPAQUE_MATERIAL - 1,
         LAST = std::numeric_limits<RenderGroupInt>::max()
     };
 
     constexpr RenderGroupInt OPAQUE_MATERIAL_ORDER_SPACE = (RenderGroupInt)RenderGroup::TRANSPARENT_MATERIAL - (RenderGroupInt)RenderGroup::OPAQUE_MATERIAL;
-    constexpr RenderGroupInt TRANSPARENT_MATERIAL_ORDER_SPACE = (RenderGroupInt)RenderGroup::SKYBOX - (RenderGroupInt)RenderGroup::TRANSPARENT_MATERIAL;
+    constexpr RenderGroupInt TRANSPARENT_MATERIAL_ORDER_SPACE = (RenderGroupInt)RenderGroup::LAST - (RenderGroupInt)RenderGroup::TRANSPARENT_MATERIAL;
     static_assert(OPAQUE_MATERIAL_ORDER_SPACE == TRANSPARENT_MATERIAL_ORDER_SPACE);
     constexpr RenderGroupInt MATERIAL_ORDER_USER_SPACE = OPAQUE_MATERIAL_ORDER_SPACE;
 
